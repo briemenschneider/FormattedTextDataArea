985a54169c2e14132df06355d605f5a90e5895bf
Brice Riemenschneider <brice.riemenschneider@gmail.com>
2020-06-21 18:48:46 +0200

    initial transformation on tutorial

--- src/visual.ts
+++ src/visual.ts
@@ -1,81 +1,91 @@
 /*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 "use strict";
 
 import "core-js/stable";
-import "./../style/visual.less";
+import "../style/visual.less";
 import powerbi from "powerbi-visuals-api";
+import IVisual = powerbi.extensibility.IVisual;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
-import IVisual = powerbi.extensibility.visual.IVisual;
 import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
-import VisualObjectInstance = powerbi.VisualObjectInstance;
-import DataView = powerbi.DataView;
-import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 
-import { VisualSettings } from "./settings";
+import * as d3 from "d3";
+type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
+
 export class Visual implements IVisual {
-    private target: HTMLElement;
-    private updateCount: number;
-    private settings: VisualSettings;
-    private textNode: Text;
+  private host: IVisualHost;
+  private svg: Selection<SVGElement>;
+  private container: Selection<SVGElement>;
+  private circle: Selection<SVGElement>;
+  private textValue: Selection<SVGElement>;
+  private textLabel: Selection<SVGElement>;
 
-    constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
-    }
+  constructor(options: VisualConstructorOptions) {
+    this.svg = d3
+      .select(options.element)
+      .append("svg")
+      .classed("circleCard", true);
+    this.container = this.svg.append("g").classed("container", true);
+    this.circle = this.container.append("circle").classed("circle", true);
+    this.textValue = this.container.append("text").classed("textValue", true);
+    this.textLabel = this.container.append("text").classed("textLabel", true);
+  }
 
-    public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
-        }
-    }
-
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return <VisualSettings>VisualSettings.parse(dataView);
-    }
-
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-    }
+  public update(options: VisualUpdateOptions) {
+    let width: number = options.viewport.width;
+    let height: number = options.viewport.height;
+    this.svg.attr("width", width);
+    this.svg.attr("height", height);
+    let radius: number = Math.min(width, height) / 2.2;
+    this.circle
+      .style("fill", "white")
+      .style("fill-opacity", 0.5)
+      .style("stroke", "black")
+      .style("stroke-width", 2)
+      .attr("r", radius)
+      .attr("cx", width / 2)
+      .attr("cy", height / 2);
+    let fontSizeValue: number = Math.min(width, height) / 5;
+    this.textValue
+      .text("Value")
+      .attr("x", "50%")
+      .attr("y", "50%")
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "middle")
+      .style("font-size", fontSizeValue + "px");
+    let fontSizeLabel: number = fontSizeValue / 4;
+    this.textLabel
+      .text("Label")
+      .attr("x", "50%")
+      .attr("y", height / 2)
+      .attr("dy", fontSizeValue / 1.2)
+      .attr("text-anchor", "middle")
+      .style("font-size", fontSizeLabel + "px");
+  }
 }
